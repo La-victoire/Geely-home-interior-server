fa0154ae35cbe9b6d7319f4f1b346b28e1789040
Victory Oghuvbu <104170646+La-victoire@users.noreply.github.com>
2025-10-07 05:36:12 +0000

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: "*",
+    origin: ['http://localhost:3000', 'https://geelyhomeinteriors.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization','X-Requested-With','Accept','Origin'],
   })
   await app.listen(process.env.PORT ?? 3000); 
 }
