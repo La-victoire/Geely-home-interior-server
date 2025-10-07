dec81ca48ff546e993a90af64a1563b83214fef5
la_victoire <oghuvbuvictory@gmail.com>
2025-10-07 06:44:53 +0100

    modified:   src/main.ts
    	deleted:    ngrok-stable-linux-amd64.zip

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://geelyhomeinteriors.vercel.app'],
+    origin: ['http://localhost:3000', 'https://geelyhomeinteriors.vercel.app','https://nonmathematical-charley-milkier.ngrok-free.dev'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization','X-Requested-With','Accept','Origin'],
+    allowedHeaders: ['Content-Type', 'Authorization','X-Requested-With','Accept','Origin', 'ngrok-skip-browser-warning',],
   })
   await app.listen(process.env.PORT ?? 3000); 
 }
